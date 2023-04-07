877a531ca50b249c1f6b1cb0be37116733d13006
Caspian <gishar@gmail.com>
2023-04-06 22:17:24 -0400

    Some EDA work

--- Learning Count Data Modeling.R
+++ Learning Count Data Modeling.R
@@ -8,16 +8,16 @@ install.packages("countreg", repos="http://R-Forge.R-project.org", type="source"
 options(digits = 3)
 options(scipen = 9)
 
-# Importing libraries
-lapply(c("tidyverse", "tidyverse", "AER", "ggplot2", "psych", "MASS"), require, character.only = T)
+#---- Importing libraries ----
+lapply(c("tidyverse", "skimr", "AER", "ggplot2", "psych", "MASS"), require, character.only = T)
 
-# Importing some datasets
+#---- Importing some datasets ----
 aadt  <- read.csv("RoadInventorysample.csv")
 aadt <- aadt[complete.cases(aadt$AADT),]
 var(aadt$AADT) / mean(aadt$AADT)
 
 
-# Trying to find some dataset with var/mean value close to 1 to fit Poisson model
+#---- Checking var/mean for dataset to help in Poisson modeling ----
 data("EquationCitations")
 ?EquationCitations
 for (i in 5:13){
@@ -33,10 +33,18 @@ boxplot(PhDPublications$articles)
 table(PhDPublications$articles)
 
 # let's go with the number of articles PhD students from the PhDPublications dataset
+
+# ---- Descriptive Statistics and EDA ----
 data("PhDPublications")
+str(PhDPublications)
 glimpse(PhDPublications)
 describe(PhDPublications)
 summary(PhDPublications)
+skim(PhDPublications)
+
+PhDPublications %>% 
+     group_by(married, gender) %>% 
+     skim(articles)                     # can leave it empty to get stats for all variables
 
 PhDPublications %>% 
      group_by(married, gender) %>% 
@@ -49,27 +57,52 @@ PhDPublications %>%
                StDev = sd(articles),
                IQR = IQR(articles),
                Counts = n(),
-               VarOverMean = var(articles)/mean(articles))
+               VarOverMean = var(articles)/mean(articles),
+               .groups = 'drop')
 
 table(PhDPublications$articles)
 
+# ---- Some histograms to see how article count is distributed
+# Histogram of number of article in the dataset
 hist(PhDPublications$articles, 
      breaks = 30,
      main = "Frequency of Article Counts",
      col = 2,
      xlab = "Number of Articles")
 
+# Histogram of number of article in the dataset grouped by gender and color coded by marital status
 ggplot(PhDPublications, 
        aes(articles)) +
      geom_bar(aes(fill=married), 
               width = 0.8,
               col = 'black') + 
-     theme(axis.text.x = element_text(angle=45, 
-                                      vjust=0.6)) + 
+     theme_bw() + 
      labs(title="Histogram of Number of articles by gender and marital status") +
      facet_wrap(~gender,
                 ncol = 1)
 
+# Histogram of number of article published by married females
+PhDPublications %>% 
+     filter(married == "yes", gender == "female") %>% 
+     ggplot(aes(x = articles,
+                fill = 'brown')) +
+     geom_histogram(binwidth = 1,
+                    col = 'black',
+                    show.legend = F,
+                    alpha = 0.6) +
+     labs(title = "Histogram of number of article published by married females",
+          x = "Articles",
+          y = "Frequency") +
+     annotate(geom="text", 
+              x=5, 
+              y=60, 
+              label=paste("Average = ", round(mean(PhDPublications$articles[PhDPublications$gender=="female" & PhDPublications$married=="yes"]),1), 
+                          "\nMedian = ", median(PhDPublications$articles[PhDPublications$gender=="female" & PhDPublications$married=="yes"]),
+                          "\nVariance = ", round(var(PhDPublications$articles[PhDPublications$gender=="female" & PhDPublications$married=="yes"]),1)),
+              color="blue4")
+     
+
+     
 glm(articles ~ ., 
     data = PhDPublications,
     family = "poisson") %>% 
